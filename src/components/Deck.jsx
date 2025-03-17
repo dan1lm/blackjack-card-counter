10c8294c21b8d82060ff589bee74b1e5b7331b2c
Danil Merinov <danilmerinov9@gmail.com>
2025-03-17 11:53:20 -0400

    writing deck logic

---
+++ src/components/Deck.jsx
@@ -0,0 +1,9 @@
+import {useState, useEffect, useRef} from 'react';
+import Card from './Card';
+import useCardCounting from '..hooks/useCardCounting';
+
+const Deck = () => {
+    return;
+}
+
+export default Deck
